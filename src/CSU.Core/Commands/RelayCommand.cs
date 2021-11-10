44fc5139b1af3fab525dedccd67087adc140a61a
Yannick Schrade <yannick1218@gmail.com>
2021-11-10 17:38:59 +0100

    Added abstract BaseCommand and RelayCommand

---
+++ src/CSU.Core/Commands/RelayCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+
+namespace CSU.Core.Commands;
+
+public class RelayCommand : BaseCommand
+{  
+
+    public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null) : base(execute, canExecute)
+    {
+    }
+
+    public override bool CanExecute(object? parameter)
+    {
+        return CanExecute != null ? CanExecute(parameter) : true;
+    }
+
+    public override void Execute(object? parameter)
+    {
+        if (CanExecute(parameter))
+            ExecuteAction(parameter);
+    }
+}
