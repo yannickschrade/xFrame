ca8dc4e4a75fca69db24975f123a0dde6afa4bbf
Yannick Schrade <42320766+yannickschrade@users.noreply.github.com>
2021-11-11 15:37:42 +0100

    Added Basic Command (#3)
    * Added abstract BaseCommand and RelayCommand
    
    * Added generic RelayCommand
    
    * Added NotifyCommand extensionmethode
    
    * Fixed DryIoC package missing

---
+++ src/CSU.Core/Commands/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Input;
+
+namespace CSU.Core.Commands;
+
+public class RelayCommand : BaseCommand
+{  
+
+    public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null) : base(execute, canExecute)
+    {
+    }
+
+    public override bool CanExecute(object? parameter)
+    {
+        return CanExecute == null || CanExecute(parameter);
+    }
+
+    public override void Execute(object? parameter)
+    {
+        if (CanExecute(parameter))
+            ExecuteAction(parameter);
+    }
+}
+
+
+public class RelayCommand<T> : RelayCommand
+{
+    public RelayCommand(Func<T?, bool> canExecute, Action<T?> execute) : base(p => execute((T?)p), p => canExecute((T?)p))
+    {
+    }
+}
