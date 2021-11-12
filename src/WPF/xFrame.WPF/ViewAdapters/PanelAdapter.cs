fff9fd17e3932dbfdb2e6af6e297459e0f887cea
Yannick Schrade <yannick1218@gmail.com>
2021-11-12 21:58:58 +0100

    Added panel adapter

---
+++ src/WPF/xFrame.WPF/ViewAdapters/PanelAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace xFrame.WPF.ViewAdapters;
+
+public class PanelAdapter : BaseViewAdapter<Panel>
+{
+    public override void InjectView(UIElement element, Panel container)
+    {
+        container.Children.Add(element);
+    }
+}
